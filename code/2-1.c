b3a6b83c703f5ed0182d3963deebd24881b37f2e
yutaeho1 <yut02@naver.com>
2026-03-13 12:32:11 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -2,6 +2,13 @@
 
 int main()
 {
+     printf("char: %u\n", sizeof(char));
+    printf("short: %u\n", sizeof(short));
+    printf("int: %u\n", sizeof(int));
+    printf("unsigned int: %u\n", sizeof(unsigned int));
+    printf("long: %u\n", sizeof(long));
+    printf("long long: %u\n", sizeof(long long));
+    printf("float: %u\n", sizeof(float));
 
     return 0;
 }
