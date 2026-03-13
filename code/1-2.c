873a7399457a4fbc0a2a76b4ab73b9ae3e6283ff
yutaeho1 <yut02@naver.com>
2026-03-13 12:12:01 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main()
 {
+    printf("%10s %10s %10s\n", "name", "id", "value");
 
+    printf("%10s %010d %10.2f\n", "kim", 12, 4.12);
+    printf("%10s %010d %10.2f\n", "lee", 1922, 3.21);
+    printf("%10s %010d %10.2f\n", "park", 432, 1.00);
 
     return 0;
 }
