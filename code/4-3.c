3717581342a0d8862310c449ef3536d23c74d239
yutaeho1 <yut02@naver.com>
2026-03-19 17:00:33 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,23 @@
 
 int main()
 {
+    int a, f = 1;
+    scanf("%d", &a);
+    for (int i = 2; i < a; i++)
+    {
+        if (a%i == 0)
+        {
+            printf("false\n");
+            int f = 0;
+            break;
+        }
 
+    }
+
+    if (f == 1)
+    {
+        printf("true\n");
+    }
     return 0;
 }
 
