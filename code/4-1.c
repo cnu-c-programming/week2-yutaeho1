5e8ddaf82f4f6c7ed64ca43489b2d79f7e4c5fd3
yutaeho1 <yut02@naver.com>
2026-03-19 16:39:13 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,36 @@
 
 int main()
 {
+    int a, b;
+    char op;
+    int result;
 
+    scanf("%d %d %c", &a, &b, &op);
+
+    switch(op) {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            if(b != 0)
+                result = a / b;
+            else {
+                printf("0으로 나눌 수 없습니다.\n");
+                return 1;
+            }
+            break;
+        default:
+            printf("잘못된 연산자입니다.\n");
+            return 1;
+    }
+
+    printf("%d\n", result);
     return 0;
 }
 
