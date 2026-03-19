46e7758e815a3662d5d58669da0b435c8b61c5a9
yutaeho1 <yut02@naver.com>
2026-03-19 16:43:01 +0900

    Update 4-2.c

--- code/4-2.c
+++ code/4-2.c
@@ -2,7 +2,15 @@
 
 int main()
 {
-
+    int count = 10;
+    for (size_t i = 1; i < count; i++)
+    {
+        for (size_t j = 1; j < count; j++)
+        {
+            printf("%d * %d = %d\n", i, j, i * j);
+        }
+        
+    }
     return 0;
 }
 
