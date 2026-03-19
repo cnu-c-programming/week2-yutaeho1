5c49551bff5c97f67fdb055e35734c3e61ffbbbe
yutaeho1 <yut02@naver.com>
2026-03-19 15:06:29 +0900

    Update 3-4.c

--- code/3-4.c
+++ code/3-4.c
@@ -2,7 +2,17 @@
 
 int main()
 {
+    int a,b,c;
 
+    a = b = c = 5;
+    printf("%d %d %d\n", a, b, c);
+
+    a = 2 + 3 * 4;
+    printf("%d %d %d\n", a, b, c);
+
+    c = a ++ + ++b;
+    printf("%d %d %d\n", a, b, c);
+    
     return 0;
 }
 
