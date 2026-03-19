cf7061a30afd475f283728dc4a762f6369882ca3
yutaeho1 <yut02@naver.com>
2026-03-19 12:58:43 +0900

    Update 2-3.c

--- code/2-3.c
+++ code/2-3.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
 int main()
-{
+{    
+    unsigned int x = 0x1234578;
+
+    unsigned char* addr = (unsigned char*)&x;
+    printf("%x\n", *addr);
+    printf("%x\n", *(addr + 1));
+    printf("%x\n", *(addr + 2));
+    printf("%x\n", *(addr + 3));
 
     return 0;
 }
