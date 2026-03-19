e128e70121e8cdeacc9d2a6910b8221d23f81928
yutaeho1 <yut02@naver.com>
2026-03-19 13:01:57 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -2,7 +2,19 @@
 
 int main()
 {
+    float a = 0.1 + 0.2;
+    float b = 0.3;
 
+    printf("a: %.100f\n", a);
+    printf("b: %.100f\n", b);
+    printf("a == b is %d\n", a == b);
+
+    double c = 0.1 + 0.2;
+    double d = 0.3;
+
+    printf("c: %.100lf\n", c);
+    printf("d: %.100lf\n", d);
+    printf("c == d is %d\n", c == d);
     return 0;
 }
 
